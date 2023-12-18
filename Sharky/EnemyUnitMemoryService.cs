5556d0534b8ced69042f78adcc6b9af35e12be3e
b <>
2023-12-18 16:34:08 +0100

    Updated EnemyUnitMemoryService

--- Sharky/EnemyUnitMemoryService.cs
+++ Sharky/EnemyUnitMemoryService.cs
@@ -2,31 +2,31 @@
 
 namespace Sharky
 {
-
     /// <summary>
     /// A service to count all units the enemy could have
     /// </summary>
     public class EnemyUnitMemoryService
     {
-        
         private ActiveUnitData ActiveUnitData;
         public Dictionary<UnitTypes, int> CurrentTotalUnits;
+
         /// <summary>
-        /// Key is the UnitType and value is a dictionary with the frame the corresponding count of total units to that time is last updated 
+        /// Key is the UnitType and value is a dictionary with the frame the corresponding count of total units to that time is last updated
         /// </summary>
         public Dictionary<UnitTypes, Dictionary<uint, int>> LastTotalUnits;
 
         public EnemyUnitMemoryService(ActiveUnitData ActiveUnitData)
         {
-            
             this.ActiveUnitData = ActiveUnitData;
-
 
             CurrentTotalUnits = new Dictionary<UnitTypes, int>();
             LastTotalUnits = new Dictionary<UnitTypes, Dictionary<uint, int>>();
+
+            UpdateEnemyCount(UnitTypes.TERRAN_SCV, 12, 1);
+            UpdateEnemyCount(UnitTypes.TERRAN_COMMANDCENTER, 12, 1);
         }
 
-        public void UpdateEnemyCount(UnitTypes type,int newCount, uint frame)
+        public void UpdateEnemyCount(UnitTypes type, int newCount, uint frame)
         {
             if (!LastTotalUnits.ContainsKey(type))
             {
@@ -37,7 +37,10 @@ namespace Sharky
             CurrentTotalUnits[type] = newCount;
         }
 
-        public void ProcessActiveUnitData(Dictionary<ulong,UnitCalculation> currentEnemyCount, uint frame)
+        public void ProcessActiveUnitData(
+            Dictionary<ulong, UnitCalculation> currentEnemyCount,
+            uint frame
+        )
         {
             Dictionary<UnitTypes, int> typeCounts = new Dictionary<UnitTypes, int>();
 
@@ -55,12 +58,14 @@ namespace Sharky
 
             foreach (var type in typeCounts.Keys)
             {
-                if (!CurrentTotalUnits.ContainsKey(type) || CurrentTotalUnits[type] < typeCounts[type])
+                if (
+                    !CurrentTotalUnits.ContainsKey(type)
+                    || CurrentTotalUnits[type] < typeCounts[type]
+                )
                 {
-                    UpdateEnemyCount(type, typeCounts[type],frame);
-                } 
+                    UpdateEnemyCount(type, typeCounts[type], frame);
+                }
             }
         }
     }
 }
-
